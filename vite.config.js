f16685ef7e4ac3480b35cc20f97010c6440b2e6c
Der Yu Tu <ann199560@gmail.com>
2026-02-11 16:51:16 +0800

    更改 vite.config 路徑

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/2023-react-daily-task-router/" : "/",
   plugins: [react()],
-})
+}));
